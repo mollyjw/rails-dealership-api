6c413f9064689bdb019ba4029d5dd628975f3834
aledure <mandrew.ledure@gmail.com>
2020-09-11 14:44:12 -0500

    added autoloader to application file to fix heroku console error

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,10 @@ module DealershipApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Test solution for heroku console error
+    config.autoloader = :classic
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
